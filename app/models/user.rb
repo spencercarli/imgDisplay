871f9125827847eeb6de28b1c234dfd5fbfb76c2
Spencer Carli <spencercarli@gmail.com>
2013-03-13 14:21:47 -0400

    Check that username and email have not been used in past

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
 
     validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
     validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+    validates_uniqueness_of :username, :email
 end
